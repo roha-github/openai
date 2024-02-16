cf8e93c70b394668e471fe9a5c7785693b454cfa
roha-github <roha.mbox@googlemail.com>
2024-02-16 12:14:55 +0100

    Create openaikey.cmd

---
+++ openaikey.cmd
@@ -0,0 +1,6 @@
+@echo off
+rem Create OpenAI account at https://platform.openai.com/ 
+rem Create API key at https://platform.openai.com/api-keys
+
+setx OPENAI_API_USR "user.name@gmail.com"
+setx OPENAI_API_KEY "sk-lxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxi"
